bb03a9dba3e1142e713953c6fbeb864efe77ddad
Rushikesh Ranganath Khandagale <khandagalerushikesh98@gmail.com>
2023-02-27 09:59:15 +0530

    Update Click4.java

--- Click4.java
+++ Click4.java
@@ -3,5 +3,6 @@ public class Click4 {
 
 	public static void main(String[] args) {
 		//write your code here..
+		WebDriver driver = new ChromeDriver();  // created an object of chrome browser
 	}
 }
